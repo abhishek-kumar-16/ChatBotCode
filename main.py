8c2aab9346273f6c01d9dadf1f8c9287dd4f070e
abhishek-kumar-16 <kumarabhishekk16dec@gmail.com>
2024-09-24 11:30:05 +0530

    Connected FastApi to Bot Demo

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI
 from fastapi import Request
 from fastapi.responses import JSONResponse
-import db_helper
-import generic_helper
+# import db_helper
+# import generic_helper
 
 app = FastAPI()
 
@@ -18,3 +18,8 @@ async def handle_request(request: Request):
     intent = payload['queryResult']['intent']['displayName']
     parameters = payload['queryResult']['parameters']
     output_contexts = payload['queryResult']['outputContexts']
+
+    if intent == "track.order:context-ordertracking":
+        return JSONResponse(content={
+            "fulfillmentText": f"Recieved {intent} in backend"
+        })

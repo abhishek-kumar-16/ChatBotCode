aedc7c53e4c74ba17af96a73db0c881b986321cd
abhishek-kumar-16 <kumarabhishekk16dec@gmail.com>
2024-09-25 14:42:03 +0530

    added add-order method

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
+from random import paretovariate
+
 from fastapi import FastAPI
 from fastapi import Request
 from fastapi.responses import JSONResponse
-# import db_helper
+import db_api
 # import generic_helper
 
 app = FastAPI()
-
-inprogress_orders = {}
 
 @app.post("/")
 async def handle_request(request: Request):
@@ -19,7 +19,37 @@ async def handle_request(request: Request):
     parameters = payload['queryResult']['parameters']
     output_contexts = payload['queryResult']['outputContexts']
 
-    if intent == "track.order:context-ordertracking":
-        return JSONResponse(content={
-            "fulfillmentText": f"Recieved {intent} in backend"
-        })
+    intent_dict={
+        'order.add:ongoing-order': addOrder,
+    # 'order.remove - context: ongoing-order': remove_from_order,
+    # 'order.complete - context: ongoing-order': complete_order,
+    'track.order:context-ordertracking': trackOrder
+    }
+
+    return intent_dict[intent](parameters)
+
+def addOrder(parameter:dict):
+    food_items=parameter["food-item"]
+    quantity=parameter["number"]
+
+    if len(food_items)!=len(quantity):
+        fullfillment_text=f"please specify the quanity for each item !"
+    else:
+        fullfillment_text=f"Recieved order in backend"
+
+    return JSONResponse(content={
+        "fulfillmentText":fullfillment_text
+    })
+
+def trackOrder(parameter:dict):
+       order_id = int(parameter['number'])
+       status=db_api.get_order_status(order_id)
+       print(status)
+       if status:
+           fulfillment_text = f"The order status for order id: {order_id} is : {status}"
+       else:
+           fulfillment_text = f"No order found with order id: {order_id}"
+
+       return JSONResponse(content={
+           "fulfillmentText": fulfillment_text
+       })
